6a2a0f72758cfaa5122fb6f04bf31abc68808236
Rupesh Chaulagain <chaulagainrupesh1@gmail.com>
2025-04-24 08:12:32 +0545

    minor changes

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -6,21 +6,21 @@ export default function Navbar() {
     <nav className="flex justify-between items-center px-40 py-4 shadow-md">
       <div className="flex items-center justify-center gap-2">
         <Image alt="novelty_lab" width={50} height={50} src="/logo.png" />
-        <h1 className="text-3xl tracking-wider text-gray-700">Novelty Lab</h1>
+        <h1 className="text-2xl tracking-wider text-gray-700">Novelty Lab</h1>
       </div>
       <div className="flex gap-20">
-        <Link href="/" className="text-lg text-gray-900">
+        <Link href="/" className="text-md text-gray-900">
           Services
         </Link>
-        <Link href="/" className="text-lg text-gray-900">
+        <Link href="/" className="text-md text-gray-900">
           About
         </Link>
-        <Link href="/" className="text-lg text-gray-900">
+        <Link href="/" className="text-md text-gray-900">
           Contact
         </Link>
       </div>
       <div>
-        <button className="bg-[#09BBc8] text-white px-5 py-3 rounded-lg font-medium cursor-pointer">
+        <button className="bg-[#09BBc8] text-white px-4 py-3 rounded-lg font-medium cursor-pointer">
           Connect With Us
         </button>
       </div>
