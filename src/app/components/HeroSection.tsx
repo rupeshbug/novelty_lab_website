8469baa1cda874a3e7ffb1d519bf24a4a769a00a
Rupesh Chaulagain <chaulagainrupesh1@gmail.com>
2025-04-26 09:40:30 +0545

    minor changes

--- src/app/components/HeroSection.tsx
+++ src/app/components/HeroSection.tsx
@@ -2,9 +2,9 @@ import { Cpu, Settings, BarChart } from "lucide-react";
 
 export default function Hero() {
   return (
-    <section className="py-20 px-40">
+    <section className="py-16 px-6 md:px-40 md:py-20">
       <div className="mx-auto flex flex-col md:flex-row items-center justify-between gap-10">
-        <div className="w-3/5 ">
+        <div className="text-center w-full md:w-3/5 md:text-left">
           <h1 className="text-4xl md:text-6xl font-bold text-gray-900 leading-tight tracking-wide mb-6">
             Supercharge Your Business with Next-Gen{" "}
             <span className="text-[#09BBc8]">AI-Powered Solutions</span>
@@ -13,7 +13,7 @@ export default function Hero() {
             Empowering businesses with AI-driven software and innovative digital
             strategies that deliver real-world impact.
           </p>
-          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 mb-10">
+          <div className="grid grid-cols-1 lg:grid-cols-3 justify-items-center gap-4 mb-10">
             <div className="flex items-center gap-3">
               <Cpu className="text-[#09bbc8] w-6 h-6" />
               <p className="text-gray-700 text-base font-medium">
@@ -33,7 +33,7 @@ export default function Hero() {
               </p>
             </div>
           </div>
-          <div className="flex gap-4">
+          <div className="flex gap-4 justify-center items-center md:justify-start">
             <button className="bg-[#09bbc8] text-white px-5 py-3 cursor-pointer rounded-lg text-base font-medium hover:bg-[#07a5b0] transition">
               Get Started
             </button>
@@ -42,7 +42,7 @@ export default function Hero() {
             </button>
           </div>
         </div>
-        <div className="w-2/5">
+        <div className="hidden lg:w-2/5">
           <img src="/img5.png" alt="hero_image" className="w-[85%] h-140" />
         </div>
       </div>
--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 export default function Navbar() {
   return (
-    <nav className="flex justify-between items-center px-6 md:px-40 py-4 shadow-md">
+    <nav className="flex justify-between items-center px-8 md:px-40 py-4 shadow-md">
       <div className="flex items-center justify-center gap-2">
         <Image alt="novelty_lab" width={50} height={50} src="/logo.png" />
         <h1 className="text-2xl tracking-wider text-gray-700">Novelty Lab</h1>
