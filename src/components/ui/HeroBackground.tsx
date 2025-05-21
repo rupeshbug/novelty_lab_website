0411825d20f004e9fcd1339cc9c7479d53d18945
Rupesh Chaulagain <chaulagainrupesh1@gmail.com>
2025-05-21 11:18:44 +0545

    finished new design of hero section

--- src/components/ui/HeroBackground.tsx
+++ src/components/ui/HeroBackground.tsx
@@ -75,44 +75,36 @@ export function HeroBackground() {
       <div className="absolute inset-0 bg-gradient-to-br from-indigo-500/[0.05] via-transparent to-rose-500/[0.05] blur-3xl z-0" />
 
       <ElegantShape
-        delay={0.3}
-        width={600}
-        height={140}
+        delay={0.5}
+        width={250}
+        height={80}
         rotate={12}
-        gradient="from-indigo-500/[0.15]"
-        className="left-[-10%] md:left-[-5%] top-[15%] md:top-[20%]"
+        gradient="from-rose-500/[0.15]"
+        className="right-[-5%] md:right-[5%] top-[70%] md:top-[80%]"
       />
       <ElegantShape
         delay={0.5}
-        width={500}
-        height={120}
-        rotate={-15}
-        gradient="from-rose-500/[0.15]"
-        className="right-[-5%] md:right-[0%] top-[70%] md:top-[75%]"
-      />
-      <ElegantShape
-        delay={0.4}
-        width={300}
+        width={250}
         height={80}
-        rotate={-8}
+        rotate={-12}
         gradient="from-violet-500/[0.15]"
         className="left-[5%] md:left-[10%] bottom-[5%] md:bottom-[10%]"
       />
       <ElegantShape
         delay={0.6}
-        width={200}
-        height={60}
+        width={150}
+        height={40}
         rotate={20}
         gradient="from-amber-500/[0.15]"
         className="right-[15%] md:right-[20%] top-[10%] md:top-[15%]"
       />
       <ElegantShape
-        delay={0.7}
+        delay={0.6}
         width={150}
         height={40}
-        rotate={-25}
+        rotate={-20}
         gradient="from-cyan-500/[0.15]"
-        className="left-[20%] md:left-[25%] top-[5%] md:top-[10%]"
+        className="left-[20%] md:left-[25%] top-[10%] md:top-[15%]"
       />
     </>
   );
