f1219c2ff0e02268b4ef867f105e19ae3cfaa763
Rupesh Chaulagain <chaulagainrupesh1@gmail.com>
2025-06-18 16:11:30 +0545

    about page started

---
+++ src/app/about/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+import Navbar from "../components/Navbar";
+
+export default function ServicesPage() {
+  return (
+    <>
+      <Navbar />
+      <main>
+        <h1 className="mt-20 text-white">About page</h1>
+      </main>
+    </>
+  );
+}
